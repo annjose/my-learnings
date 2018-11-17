7ebca44cfcab85401f1df120e744a31916448d13
Ann Catherine Jose <ann.jose@gmail.com>
2018-11-17 13:20:15 -0800

    Initial version of sample app (simple project without Cocoapods)

---
+++ URLSessionMockerSample/URLSessionMockerSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URLSessionMockerSample
+//
+//  Created by Jose, Ann Catherine on 11/17/18.
+//  Copyright © 2018 Chiramattel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ URLSessionMockerSample/URLSessionMockerSampleTests/URLSessionMockerSampleTests.swift
@@ -0,0 +1,27 @@
+//
+//  URLSessionMockerSampleTests.swift
+//  URLSessionMockerSampleTests
+//
+//  Created by Jose, Ann Catherine on 11/17/18.
+//  Copyright © 2018 Chiramattel. All rights reserved.
+//
+
+import XCTest
+@testable import URLSessionMockerSample
+
+//import Mocker
+
+class URLSessionMockerSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_get_date_success() {
+        XCTAssert(1==1)
+    }
+}
