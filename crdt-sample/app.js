2b3d108498c28ff36599670324575f6198375f9f
Ann Catherine Jose <ann.jose@gmail.com>
2020-10-17 17:34:31 -0700

    Try Automerge APIs to add, merge and conflict.

---
+++ crdt-sample/app.js
@@ -0,0 +1,59 @@
+var message = 'Hello World'
+console.log(message);
+
+const Automerge = require('automerge')
+
+let doc1 = Automerge.from( { todos: [] } )
+
+doc1 = Automerge.change(doc1, 'Add first Todo through doc1', doc => {
+    doc.todos.push({ title: 'Task 1', done: false})
+})
+console.log('doc1......')
+display(doc1)
+
+doc1 = Automerge.change(doc1, 'Add second todo through doc1', doc => {
+    doc.todos.push({ title: 'Task 2', done: false })
+})
+console.log('doc1......')
+display(doc1)
+
+// create another doc (simulating updates from another device)
+let doc2 = Automerge.init()
+doc2 = Automerge.merge(doc2, doc1)
+console.log('doc2......')
+display(doc2)
+
+// Fun starts
+doc1 = Automerge.change(doc1, 'Mark first todo as done through doc1', doc => {
+    doc.todos[0].done = true
+})
+doc1 = Automerge.change(doc1, 'setting title as New through doc1', doc => {
+    doc.todos[0].title = 'New title [doc1]'
+})
+console.log('doc1......')
+display(doc1)
+
+doc2 = Automerge.change(doc2, 'setting title as Conflict through doc2', doc => {
+    // doc.todos.deleteAt
+    doc.todos[0].title = 'Conflict title for todo1 [doc2]'
+    doc.todos[1].title = 'Conflict title for todo2 [doc2]'
+})
+console.log('doc2......')
+display(doc2)
+
+console.log('finalDoc....')
+let finalDoc = Automerge.merge(doc1, doc2) // doc1 wins conflict resolve
+//let finalDoc = Automerge.merge(doc2, doc1) // doc2 wins conflict resolve
+display(finalDoc)
+console.log(Automerge.getHistory(finalDoc).map(state => [state.change.message, state.snapshot.todos.length]))
+
+function display(doc) {
+
+    console.log('ActorId: ' +  Automerge.getActorId(doc) + ' | todos count: ' + doc.todos.length)
+
+    doc.todos.forEach(element => {
+        console.log(element); 
+    });
+   
+    console.log('-------------------------------')
+}
